d6079a683775b037d0cb0217175f2e56460ebccc
Lattapan <noey.rachata@gmail.com>
2023-02-24 17:59:40 +0700

    Change images patch

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,14 +1,13 @@
+/** @format */
+
 import React from "react";
+import Icon from "../images/pisa-icon.png";
 
 export default function Navbar() {
-    return (
-        <div className="nav">
-            <img
-                src="../images/pisa-icon.png"
-                alt="Pisa icon"
-                className="nav--icon"
-            />
-            <p className="nav--title">My Italy Travel Journal</p>
-        </div>
-    );
+  return (
+    <div className="nav">
+      <img src={Icon} alt="Pisa icon" className="nav--icon" />
+      <p className="nav--title">My Italy Travel Journal</p>
+    </div>
+  );
 }
--- src/data.jsx
+++ src/data.jsx
@@ -1,38 +1,44 @@
+/** @format */
+
+import Venice from "../images/venice.jpg";
+import Pisa from "../images/pisa.jpg";
+import Florence from "../images/florence.jpg";
+
 export default [
-    {
-        id: 1,
-        title: "Ponte della Misericordia",
-        location: "Venice",
-        googleMapsUrl:
-            "https://www.google.com/maps/place/Ponte+della+Misericordia/@45.4430418,12.3322688,17z/data=!3m1!4b1!4m5!3m4!1s0x477eb19c42b5b26d:0x44fd3fe5687f9ee4!8m2!3d45.4430381!4d12.3344575",
-        startDate: "25 July, 2022",
-        endDate: "27 July, 2022",
-        description:
-            "Ponte della Misericordia is a lovely and quiet spot to take nice photos. Yes, Venice is a fairly small island. But, I think you still need two days to explore the island. And this bridge at the back of the island is just perfect. I do recommend people to start their day early -- that is also the trick for most places, if you want to avoid the crowd.",
-        image: "../images/venice.jpg",
-    },
-    {
-        id: 2,
-        title: "Piazzale Michelangelo",
-        location: "Florence",
-        googleMapsUrl:
-            "https://www.google.com/maps/place/Piazzale+Michelangelo/@43.7629353,11.2628674,807m/data=!3m2!1e3!4b1!4m5!3m4!1s0x132a53f743663843:0xb4f768e1820d5119!8m2!3d43.7629314!4d11.2650561",
-        startDate: "30 Jul, 2022",
-        endDate: "2 Aug, 2022",
-        description:
-            "If you want to see the best view of Florence, I recommend going to Piazzale Michelangelo. The vibe at the plaza during sunset is also unbeatable. People gathering around the place watching the sunset together. Some people dancing and singing. You can walk down to the city after dawn, too. It was one of the highlights of my trip.",
-        image: "../images/florence.jpg",
-    },
-    {
-        id: 3,
-        title: "Leaning Tower of Pisa",
-        location: "Pisa",
-        googleMapsUrl:
-            "https://www.google.com/maps/place/Leaning+Tower+of+Pisa/@43.7229559,10.3944083,808m/data=!3m2!1e3!4b1!4m5!3m4!1s0x12d591a6c44e88cd:0x32eca9b1d554fc03!8m2!3d43.722952!4d10.3965975",
-        startDate: "1 Aug, 2022",
-        endDate: "1 Aug, 2022",
-        description:
-            "The Leaning Tower of Pisa was a must side trip I had to do while visiting Florence. I have to admit that my main goal was to take a cool photo, and I must say that I am satisfied with the outcome. All credits to my beautiful friend Avi, though. Side note: I had the best pistachio gelato on my walk to the tower.",
-        image: "../images/pisa.jpg",
-    },
+  {
+    id: 1,
+    title: "Ponte della Misericordia",
+    location: "Venice",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Ponte+della+Misericordia/@45.4430418,12.3322688,17z/data=!3m1!4b1!4m5!3m4!1s0x477eb19c42b5b26d:0x44fd3fe5687f9ee4!8m2!3d45.4430381!4d12.3344575",
+    startDate: "25 July, 2022",
+    endDate: "27 July, 2022",
+    description:
+      "Ponte della Misericordia is a lovely and quiet spot to take nice photos. Yes, Venice is a fairly small island. But, I think you still need two days to explore the island. And this bridge at the back of the island is just perfect. I do recommend people to start their day early -- that is also the trick for most places, if you want to avoid the crowd.",
+    image: Venice,
+  },
+  {
+    id: 2,
+    title: "Piazzale Michelangelo",
+    location: "Florence",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Piazzale+Michelangelo/@43.7629353,11.2628674,807m/data=!3m2!1e3!4b1!4m5!3m4!1s0x132a53f743663843:0xb4f768e1820d5119!8m2!3d43.7629314!4d11.2650561",
+    startDate: "30 Jul, 2022",
+    endDate: "2 Aug, 2022",
+    description:
+      "If you want to see the best view of Florence, I recommend going to Piazzale Michelangelo. The vibe at the plaza during sunset is also unbeatable. People gathering around the place watching the sunset together. Some people dancing and singing. You can walk down to the city after dawn, too. It was one of the highlights of my trip.",
+    image: Florence,
+  },
+  {
+    id: 3,
+    title: "Leaning Tower of Pisa",
+    location: "Pisa",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Leaning+Tower+of+Pisa/@43.7229559,10.3944083,808m/data=!3m2!1e3!4b1!4m5!3m4!1s0x12d591a6c44e88cd:0x32eca9b1d554fc03!8m2!3d43.722952!4d10.3965975",
+    startDate: "1 Aug, 2022",
+    endDate: "1 Aug, 2022",
+    description:
+      "The Leaning Tower of Pisa was a must side trip I had to do while visiting Florence. I have to admit that my main goal was to take a cool photo, and I must say that I am satisfied with the outcome. All credits to my beautiful friend Avi, though. Side note: I had the best pistachio gelato on my walk to the tower.",
+    image: Pisa,
+  },
 ];
